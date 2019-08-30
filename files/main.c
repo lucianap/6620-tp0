a3b60fbe213a02334886eb6d05e4437f58151b6e
Luciana <lucianasl@gmail.com>
2019-08-30 17:04:43 -0300

    chaged to snake_case

--- files/main.c
+++ files/main.c
@@ -6,11 +6,11 @@
 
 //Lee del standar input una cadena de longitud variable y la devuelve.
 //Almacena el tamaño total del buffer de la cadena en size.
-char* readFromStdIn(size_t* size);
+char* read_from_stdin(size_t* size);
 
 //Devuelve las matrices como un array de doubles, pone el tamaño de las matrices en la variable matrixSize.
-//Formato esperado: TamañoMatriz a11 .... ann b11 .... bnn
-//Ejemplo: 2 1.1 2.7 3 4.3 1.23 6 12 1
+//  Formato esperado: TamañoMatriz a11 .... ann b11 .... bnn
+//  Ejemplo: 2 1.1 2.7 3 4.3 1.23 6 12 1
 //Casos de error:
 //  1: tamaño de las matrices inválido: se pone un número no entero como tamaño
 //  2: valores inválidos: solo se aceptan floats en notación científica.
@@ -20,29 +20,29 @@ double* parse(char* stringMatrixes, size_t* matrixSize);
 
 //Extrae la matriz [matrixNum] del array parseado.
 //Solo se admite matrixNum = 0 (primera matriz) o matrixNum = 1 (segunda matriz)
-double* extractMatrix(double* arrayMatrixes, size_t matrixSize, size_t matrixNum);
+double* extract_matrix(double* array_matrixes, size_t matrix_size, size_t matrix_num);
 
 int main()
 {
     size_t size;
-    char* stringRead = readFromStdIn(&size);
+    char* string_read = read_from_stdin(&size);
     
-    size_t matrixSize;
-    double* matrixes = parse(stringRead, &matrixSize);
+    size_t matrix_size;
+    double* matrixes = parse(string_read, &matrix_size);
     
-    double* matrix1 = extractMatrix(matrixes, matrixSize, 0);
-    double* matrix2 = extractMatrix(matrixes, matrixSize, 1);
+    double* matrix1 = extract_matrix(matrixes, matrix_size, 0);
+    double* matrix2 = extract_matrix(matrixes, matrix_size, 1);
 
-    printf("\nSize de las matrices:%ld \n\n",matrixSize);
+    printf("\nSize de las matrices:%ld \n\n",matrix_size);
 
     printf("\nSe imprime la matriz 1 \n\n");
     int i;
-    for(i = 0; i < (matrixSize*matrixSize); i++){
+    for(i = 0; i < (matrix_size*matrix_size); i++){
         printf("matrix1[%i]=%f \n", i, matrix1[i]);
     }
 
     printf("\nSe imprime la matriz 2 \n\n");
-    for(i = 0; i < (matrixSize*matrixSize); i++){
+    for(i = 0; i < (matrix_size*matrix_size); i++){
         printf("matrix2[%i]=%f \n", i, matrix2[i]);
     }
 
@@ -50,12 +50,12 @@ int main()
     free(matrixes);
     free(matrix1);
     free(matrix2);    
-    free(stringRead);
+    free(string_read);
     return 0;
 
 }
 
-char* readFromStdIn(size_t* size)
+char* read_from_stdin(size_t* size)
 {
     unsigned int len_max = 128;
     unsigned int current_size = 0;
@@ -96,23 +96,23 @@ char* readFromStdIn(size_t* size)
 
 }
 
-double* parse(char* stringRead, size_t* matrixSize) {
+double* parse(char* string_read, size_t* matrix_size) {
 
     //cantidad de matrices en el string.
     //TODO - evaluar si tal vez este dato podría venir esto por param.
-    int matrixCount = 2;
+    int matrix_count = 2;
 
     //Puntero en el que se almacena cada caracter leído.
     char *p;
-    p = strtok(stringRead, " ");
+    p = strtok(string_read, " ");
 
     double* matrixes = NULL;
 
     //Primer caracter: tamaño de las matrices
-    if((*matrixSize) = atoi(p)){
+    if((*matrix_size) = atoi(p)){
 
         //aloco memoria para las matrices
-        int n = (*matrixSize)*(*matrixSize)*matrixCount;
+        int n = (*matrix_size)*(*matrix_size)*matrix_count;
         matrixes = malloc(n*sizeof(double));
 
         //declaro un iterador entero para significar los índices del array de resultado.
@@ -122,10 +122,10 @@ double* parse(char* stringRead, size_t* matrixSize) {
         {
 
             //valor parseado del string
-            double matrixValueij;
+            double matrix_valueij;
 
-            if(matrixValueij = atof(p)){
-                matrixes[iterator] = matrixValueij;
+            if(matrix_valueij = atof(p)){
+                matrixes[iterator] = matrix_valueij;
                 iterator++;
             } else {
 
@@ -150,25 +150,25 @@ double* parse(char* stringRead, size_t* matrixSize) {
 }
 
 
-double* extractMatrix(double* arrayMatrixes, size_t matrixSize, size_t matrixNum) {
+double* extract_matrix(double* array_matrixes, size_t matrix_size, size_t matrix_num) {
     
     int i;
     
-    if(matrixNum > 1) {
+    if(matrix_num > 1) {
         fprintf( stderr, "La cantidad de matrices permitidas son 2.\n");
     }
     
-    double* matrixN = malloc(matrixSize*matrixSize*sizeof(double));
-    int matrixIndex = 0;
+    double* matrix_n = malloc(matrix_size*matrix_size*sizeof(double));
+    int matrix_index = 0;
    
-    for(i = 0; i < matrixNum; i++ ) {
+    for(i = 0; i < matrix_num; i++ ) {
         //Se avanza el indicador de a una matriz.
-        matrixIndex+=(matrixSize*matrixSize);
+        matrix_index+=(matrix_size*matrix_size);
     }
 
-    for(i = 0; i<(matrixSize*matrixSize); i++, matrixIndex++) {
-        matrixN[i] = arrayMatrixes[matrixIndex];
+    for(i = 0; i<(matrix_size*matrix_size); i++, matrix_index++) {
+        matrix_n[i] = array_matrixes[matrix_index];
     }
 
-    return matrixN;
+    return matrix_n;
 }
